79f848cb8a3a9b7f66436d6d09ba3304d1ae91b9
Andrew Fray <andrew.fray@gmail.com>
2019-12-13 23:38:53 +0000

    day12 pt1

--- day12.py
+++ day12.py
@@ -36,6 +36,16 @@ class Moons:
         self.step_gravity()
         self.step_velocity()
 
+    def sum_energy(self):
+        tot_energy = 0
+        for i in range(len(self.positions)):
+            pos = self.positions[i]
+            vel = self.velocities[i]
+            pot_energy = abs(pos[0]) + abs(pos[1]) + abs(pos[2])
+            kin_energy = abs(vel[0]) + abs(vel[1]) + abs(vel[2])
+            tot_energy += pot_energy * kin_energy
+        return tot_energy
+
     def test_grav(self, expected_vels, msg):
         self.step_gravity()
         assert self.velocities == expected_vels, "%s expected %s but got %s"%(msg, str(expected_vels), str(self.velocities))
@@ -49,6 +59,10 @@ class Moons:
             self.step()
         assert self.positions == expected_pos, "%s expected pos %s but got %s"%(msg, str(expected_pos), str(self.positions))
         assert self.velocities == expected_vels, "%s expected vel %s but got %s"%(msg, str(expected_vels), str(self.velocities))
+
+    def test_energy(self, expected_energy, msg):
+        actual_energy = self.sum_energy()
+        assert actual_energy == expected_energy, "%s expected %d got %d"%(msg, expected_energy, actual_energy)
 
 def test():
     Moons([(-1,0,0),(0,0,0)]).test_grav([(1,0,0),(-1,0,0)], "x moves towards")
@@ -70,6 +84,30 @@ def test():
                     [(2,1,-3),(1,-8,0),(3,-6,1),(2,0,4)],
                     [(-3,-2,1),(-1,1,3),(3,2,-3),(1,-1,-1)],
                     "step10")
+
+    Moons([]).test_energy(0, "no moons no energy")
+    Moons([(1,2,1)],[(1,0,0)]).test_energy(4, "summed potential")
+    Moons([(-1,2,1)],[(1,0,0)]).test_energy(4, "summed abs potential")
+    Moons([(1,0,0)],[(1,2,1)]).test_energy(4, "summed kinetic")
+    Moons([(1,0,0)],[(-1,2,1)]).test_energy(4, "summed abs kinetic")
+    Moons([(2,0,0)],[(3,0,0)]).test_energy(6, "mult kin/pots energy")
+    Moons([(2,0,0),(5,0,0)],[(3,0,0),(7,0,0)]).test_energy(41, "sum all planet energy")
+
+    test2 = Moons([(-8,-10,0),(5,5,10),(2,-7,3),(9,-8,-3)])
+    test2.test_step(100,
+                    [(8,-12,-9),(13,16,-3),(-29,-11,-1),(16,-13,23)],
+                    [(-7,3,0),(3,-11,-5),(-3,7,4),(7,1,1)],
+                    "test2 100")
+    test2.test_energy(1940, "test2 energy")
+    
     print("tests pass")
 
 test()
+
+puzzle = Moons([(-7,-1,6),
+                (6,-9,-9),
+                (-12,2,-7),
+                (4,-17,-12)])
+for i in range(1000):
+    puzzle.step()
+print(puzzle.sum_energy())
